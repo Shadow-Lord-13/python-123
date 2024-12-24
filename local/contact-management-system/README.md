6c18127c26338bcc126ca275a6e9f28035ca104a
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2024-12-24 21:28:16 +0530

    Adding code for ADD and VIEW contacts

---
+++ local/contact-management-system/README.md
@@ -0,0 +1,15 @@
+Contact Management System
+
+Description: A CLI app to manage contacts with attributes like name, email, and phone number.
+
+Technologies: Python (with sqlite3 for persistence)
+
+CRUD Operations:
+- Create: Add a new contact.
+- Read: View all or search for specific contacts.
+- Update: Modify contact details.
+- Delete: Remove a contact.
+
+Learning Goals:
+- Work with SQLite databases.
+- Use basic SQL queries within Python.
---
+++ local/contact-management-system/main.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+def display_options():
+    print(
+    "\nOptions available :\n"
+    "1 -> Add Contact\n"
+    "2 -> Edit Contact\n"
+    "3 -> Delete Contact\n"
+    "4 -> View Contact\n"
+    "5 -> Exit App\n"
+)
+
+def create_database():
+    con = sqlite3.connect("contacts.db")
+    cur = con.cursor()
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS contacts (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    firstname TEXT NOT NULL,
+                    lastname TEXT NOT NULL,
+                    phonenumber TEXT UNIQUE NOT NULL
+                )
+            ''')
+    
+    # cur.execute("DROP TABLE contacts")
+
+    con.commit()
+    con.close()
+
+def add_contact():
+    con = sqlite3.connect("contacts.db")
+    cur = con.cursor()
+    first_name = input("Please enter the first name: ")
+    last_name = input("Please enter the last name: ")
+    phone_number = input("Please enter the phone number: ")
+    
+    try:
+        cur.execute("INSERT INTO contacts(firstname, lastname, phonenumber) VALUES (?,?,?)", (first_name, last_name, phone_number))
+        con.commit()
+        print("\nContact added succesfully")
+        con.close()
+    except sqlite3.IntegrityError:
+        print("\nPhone number already present!")
+
+def view_contacts():
+    con = sqlite3.connect("contacts.db")
+    cur = con.cursor()
+
+    print("\nContacts:")
+    res = cur.execute("SELECT * FROM contacts")
+    print(res.fetchall())
+
+    con.commit()
+    con.close()
+
+
+def main():
+    create_database()
+    print("Welcome to Contact Management System")
+
+    while True:
+        try:
+            display_options()
+            user_input = int(input("Please select from the available options: "))
+
+            if user_input == 1:
+                add_contact()
+            elif user_input == 4:
+                view_contacts()
+            elif user_input == 5:
+                print("\nExiting App: Thank you for using our App!!")
+                break
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
